e673be7efc15c8e3d44c612ca5e8d195cb7d106c
damendozac <damendozac@unal.edu.co>
2021-12-07 17:39:45 -0500

    Definición listas enlazadas

--- punto-1.py
+++ punto-1.py
@@ -1,4 +1,39 @@
-if __name__ == '__main__':
-    t = input() #Numero casos de prueba
-    n,k=map(int,input().split()) # N = Cantidad de cartas y K = Cantidad de jugadores
-    print (t,n,k)
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class LinkedList: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+        if head == None:
+            nextNode = Node(data)
+            return nextNode
+        elif head.next == None:
+            nextNode = Node(data)
+            head.next = nextNode
+            return head
+        else:
+            self.insert(head.next, data)
+	
+
+t = input() #Numero casos de prueba
+nAndK= input().split()
+n = int(nAndK[0]) # N = Cantidad de cartas y K = Cantidad de jugadores
+k = int(nAndK[1])
+jugadores = LinkedList()
+head=None
+print(n, k)
+for i in range(n+1):
+    head = jugadores.insert(head, i+1)
+jugadores.display(head)
+
+# for i in range(T):
+#     data=int(input())
+#     head=mylist.insert(head,data)    
+#     mylist.display(head); 
+      
